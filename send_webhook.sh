c49a9a6c37b6d3ec4ef3f94d96a447db45425a84
AbelPouillet <abelix.pouillet@gmail.com>
2025-04-30 18:56:59 +0200

    correction

--- send_webhook.sh
+++ send_webhook.sh
@@ -7,7 +7,7 @@ WEBHOOK_URL="$NGROK_URL/webhook-test/6a454f38-091e-44be-ba6f-eeb5b8dc9deb"  # Re
 LAST_COMMIT=$(git log -1 --pretty=format:"%H" 2>/dev/null || echo "No commit found")
 COMMIT_MESSAGE=$(git log -1 --pretty=format:"%s" 2>/dev/null || echo "No commit message")
 AUTHOR=$(git log -1 --pretty=format:"%an" 2>/dev/null || echo "No author")
-DIFF=$(git diff HEAD~1 HEAD>/dev/null || echo "No changes")
+DIFF=$(git diff HEAD~1 HEAD 2>/dev/null || echo "No changes")
 
 # Construire le JSON
 JSON=$(jq -n \
