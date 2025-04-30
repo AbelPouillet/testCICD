d850ab4648cf0bb40e3a37e1b4e752ea9ca2df6d
AbelPouillet <abelix.pouillet@gmail.com>
2025-04-30 19:36:54 +0200

    on change de commande dans un dernier espoir

--- send_webhook.sh
+++ send_webhook.sh
@@ -6,10 +6,8 @@ NGROK_URL="https://5a08-2a01-e0a-ba9-4c40-358b-dff0-42e2-58c8.ngrok-free.app"
 WEBHOOK_URL="$NGROK_URL/webhook-test/6a454f38-091e-44be-ba6f-eeb5b8dc9deb"  # Remplacez par votre URL n8n
 
 LAST_COMMIT=$(git log -1 --pretty=format:"%H")
-PREVIOUS_COMMIT=$(git log -2 --pretty=format:"%H" | tail -n1) # Récupère l'avant-dernier commit
-COMMIT_MESSAGE=$(git log -1 --pretty=format:"%s" 2>/dev/null || echo "No commit message")
 AUTHOR=$(git log -1 --pretty=format:"%an" 2>/dev/null || echo "No author")
-DIFF=$(git diff $PREVIOUS_COMMIT $LAST_COMMIT)
+DIFF=$(git show HEAD)
 # Construire le JSON
 JSON=$(jq -n \
   --arg commit "$LAST_COMMIT" \
