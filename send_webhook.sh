f28e01844c5de89498700eed4988e56a4a176175
AbelPouillet <abelix.pouillet@gmail.com>
2025-05-01 13:56:28 +0200

    git show

--- send_webhook.sh
+++ send_webhook.sh
@@ -6,10 +6,10 @@ NGROK_URL="https://f5f9-2a01-e0a-ba9-4c40-ec81-ceb9-b8ac-cf33.ngrok-free.app"
 WEBHOOK_URL="$NGROK_URL/webhook-test/6a454f38-091e-44be-ba6f-eeb5b8dc9deb"  # Remplacez par votre URL n8n
 
 LAST_COMMIT=$(git log -1 --pretty=format:"%H")
-FILES_CHANGED=$(git log --name-status)
+FILES_CHANGED=$(git show --name-status --oneline "$LAST_COMMIT")
 COMMIT_MESSAGE=$(git log -1 --pretty=format:"%s")
 AUTHOR=$(git log -1 --pretty=format:"%an" 2>/dev/null || echo "No author")
-DIFF=$(git log -p)
+DIFF=$(git show "$LAST_COMMIT")
 # Construire le JSON
 JSON=$(jq -n \
   --arg commit "$LAST_COMMIT" \
