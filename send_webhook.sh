237e4c09d51771ab828fea0393bb486a172f4b71
AbelPouillet <abelix.pouillet@gmail.com>
2025-05-01 13:16:37 +0200

    ajout du suivie des fichiers modifier

--- send_webhook.sh
+++ send_webhook.sh
@@ -6,6 +6,7 @@ NGROK_URL="https://f5f9-2a01-e0a-ba9-4c40-ec81-ceb9-b8ac-cf33.ngrok-free.app"
 WEBHOOK_URL="$NGROK_URL/webhook-test/6a454f38-091e-44be-ba6f-eeb5b8dc9deb"  # Remplacez par votre URL n8n
 
 LAST_COMMIT=$(git log -1 --pretty=format:"%H")
+FILES_CHANGED=$(git log --name-status)
 COMMIT_MESSAGE=$(git log -1 --pretty=format:"%s")
 AUTHOR=$(git log -1 --pretty=format:"%an" 2>/dev/null || echo "No author")
 DIFF=$(git log -p)
@@ -15,11 +16,13 @@ JSON=$(jq -n \
   --arg message "$COMMIT_MESSAGE" \
   --arg author "$AUTHOR" \
   --arg diff "$DIFF" \
+  --arg files "$FILES_CHANGED" \
   '{
     commit: $commit,
     message: $message,
     author: $author,
-    diff: $diff
+    diff: $diff,
+    files: $files
   }')
 
 # Envoyer la requête POST
